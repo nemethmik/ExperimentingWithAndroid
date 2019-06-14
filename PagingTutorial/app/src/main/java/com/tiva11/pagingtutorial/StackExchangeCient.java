98f8265b45ae529450280a30222b5439d985b1dc
Miklos Nemeth <nemethmiklos1960@gmail.com>
2019-06-14 22:13:00 +0200

    StackExchangeClient made abstract to prevent instantiating

--- PagingTutorial/app/src/main/java/com/tiva11/pagingtutorial/StackExchangeCient.java
+++ PagingTutorial/app/src/main/java/com/tiva11/pagingtutorial/StackExchangeCient.java
@@ -3,7 +3,7 @@ package com.tiva11.pagingtutorial;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class StackExchangeCient {
+public abstract class StackExchangeCient {
     private static final String BASE_URL = "https://api.stackexchange.com/2.2/";
     private static Retrofit httpClient = new Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()).build();
